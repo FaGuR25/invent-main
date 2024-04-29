9c09d2d1ac9bc0f39b7ef4b712b7e911a48ccee2
Fatima <guadalupe20050@gmail.com>
2024-04-29 14:30:23 -0600

    css

--- invent-main/apps/screen/ProductDetail.tsx
+++ invent-main/apps/screen/ProductDetail.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Product} from './model/Products';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import {RouteProp} from '@react-navigation/native';
 import {RootStackParamList} from '../../App';
 import {StackNavigationProp} from '@react-navigation/stack';
@@ -23,13 +23,46 @@ function ProductsDetail({route}: Props): React.JSX.Element {
   return (
     <SafeAreaView>
       {product && (
-        <View>
-          <Text>{product.nombre}</Text>
-          <Text>{product.currentStock}</Text>
-          <Text>{product.precio}</Text>
+        <View style={styles.page}>
+          <Text style={styles.header}>{product.nombre}</Text>
+          <View style={styles.row}>
+            <Text style={styles.col}> Existencias:</Text>
+            <Text style={styles.colAuto}>
+              {product.currentStock} / {product.maxStock}
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.col}>precio</Text>
+            <Text style={styles.colAuto}> {product.precio}</Text>
+          </View>
         </View>
       )}
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 40,
+    color: 'black',
+  },
+  precio: {
+    fontSize: 40,
+    color: 'black',
+  },
+  existencia: {
+    fontSize: 25,
+    color: 'black',
+  },
+  row: {
+    display: 'flex',
+  },
+  col: {
+    flexGrow: 999,
+  },
+  colAuto: {},
+  page:{
+    
+  }
+});
 export default ProductsDetail;
